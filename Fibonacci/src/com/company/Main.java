0bc332648a8f305fac279102811036f5536a7757
arunvyas168 <arunvyas27@gmail.com>
2016-08-27 11:56:26 -0700

    O(n) solution with memoization with O(n) space

--- Fibonacci/src/com/company/Main.java
+++ Fibonacci/src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        int number = 5;
+        int number = 8;
         generateFibonacci(number);
         System.out.println();
         int start = 1;
@@ -11,6 +14,35 @@ public class Main {
             System.out.print(recursiveGenerateFib(start)+" ");
             start++;
         }
+
+        System.out.println();
+        int[] memo = new int[number+1];
+        start = 1;
+        while(start<=number){
+            System.out.print(recursiveFib_memo(start,memo)+" ");
+            start++;
+        }
+        System.out.println();
+
+    }
+
+    //memoization
+    public static int recursiveFib_memo(int number, int[] memo){
+
+        if (number == 0){
+            memo[number] = 0;
+            return 0;
+        }else if (number == 1){
+            memo[number] = 1;
+            return 1;
+        }else {
+            if(memo[number]==0){
+                return memo[number] = (recursiveFib_memo(number-1, memo)+recursiveFib_memo(number-2, memo));
+            }else {
+                return memo[number];
+            }
+        }
+
     }
 
     public static void generateFibonacci(int number){
@@ -35,5 +67,4 @@ public class Main {
             return(recursiveGenerateFib(number-1)+recursiveGenerateFib(number-2));
         }
     }
-
 }
